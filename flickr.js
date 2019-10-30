3e84e692116a5e792bcfb9a8a507b969bc49a919
Ulibka <gena68@inbox.ru>
2019-10-30 15:55:44 +0300

    выделена функция поиска

--- flickr.js
+++ flickr.js
@@ -24,11 +24,13 @@ cats = Object {
 }
 */
 
+const getApiKey = () => '5989617c0a5018ae4ee610b0c8b25e0e';
+
 function flickrTagSearchF(tag) {
 
     // Returns a promise that resolves after _seconds_
     const delay = seconds => new Promise(resolve => setTimeout(resolve, seconds * 1000));
-    const getApiKey = () => '5989617c0a5018ae4ee610b0c8b25e0e';
+    
 
     // Returns a Promise that resolves to an array of urls to images that
     // represents a (paged) result of a tag search on Flickr.
@@ -107,4 +109,69 @@ function flickrTagSearchF(tag) {
     }
 }
 
+
+// параметры вызова поиска фотографий описаны тут :
+// https://www.flickr.com/services/api/explore/flickr.photos.search
+// функция возвращает promise
+ function flickrTagSearchPromice (tag, page=1) {
+  const apiKey = getApiKey();
+  return fetch(
+      'https://api.flickr.com/services/rest/' +
+      '?method=flickr.photos.search' +
+      '&api_key=' + apiKey +
+      '&page=' + page +
+      '&per_page=20'+
+      '&tags=' + tag +
+      // '&text=' + tag +
+      '&content_type=1'+
+      '&sort=relevance'+
+      '&in_gallery=true'+
+      // '&is_commons=true'+
+      '&tag_mode=AND' +
+      '&format=json' +
+      '&nojsoncallback=1'
+  )     
+  .then(response => { 
+    if (response.ok)  { // если HTTP-статус в диапазоне 200-299
+        // получаем тело ответа 
+        return response.json(); // json тоже вернет promice
+            // .then (data => {
+            
+            // Пример вывода
+            // photos:
+            //     page: 1
+            //     pages: 55218
+            //     perpage: 20
+            //     photo: (20) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
+            //     total: "1104341"
+            // stat: "ok"
+
+            // Один элементЖ
+            // 0:
+            // farm: 66
+            // id: "48971887813"
+            // isfamily: 0
+            // isfriend: 0
+            // ispublic: 1
+            // owner: "16854222@N05"
+            // secret: "efdce9d68c"
+            // server: "65535"
+            // title: "Paddling Towards New Adventures"
+
+            //     console.log(data);
+            //     console.log(data.photos.photo.length);
+            //     console.log(data.photos.photo[0]);
+            //     return data;
+            // });
+
+    } else {
+      alert('Ошибка HTTP :' + response.status);
+    }
+ })
+.catch(err => {console.log('Fetch Error :',err)});
+
+}
+
+
+// https://javascript.info/async-iterators-generators
 
